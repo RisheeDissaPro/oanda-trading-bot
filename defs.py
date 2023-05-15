599d568e8f9e31fc34dfd84a33407c69a119b419
RisheeDissaPro <rishee.dissanayake.pro@gmail.com>
2023-05-15 15:56:34 +0530

    First commit

---
+++ defs.py
@@ -0,0 +1,7 @@
+API_KEY = "d6f7e48dec6a189371611760f36ac9f1-401eac2c18083820fd46859a45c18537"
+ACCOUNT_ID = "101-001-25675267-001"
+OANDA_URL = "https://api-fxpractice.oanda.com/v3"
+
+SECURE_HEADER = {
+    'Authorization': f'Bearer {API_KEY}'
+}
